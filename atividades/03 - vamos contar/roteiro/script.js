a4721eb28d816c07fe6fc42bbde3999442adde6d
Erick Alexandre <111152377+kinhoown@users.noreply.github.com>
2023-07-10 21:06:05 -0300

    att
    em andamento.

--- atividades/03 - vamos contar/roteiro/script.js
+++ atividades/03 - vamos contar/roteiro/script.js
@@ -1,9 +1,18 @@
-var inicio = document.getElementById('inicio')
-var fim = document.getElementById('fim')
-var passo = document.getElementById('passo')
-var res = document.getElementById('res')
 
-function contar(){
-    res.innerHTML = `${inicio.value} ${fim.value} ${passo.value}`
+
+
+function conta() {
+    var inicio = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+    var res = document.getElementById('res')
+
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        alert('[ERRO] Faltam dados!')
+    }else{
+        for (inicio.value ; inicio.value < fim ; inicio.value =+ 1){
+            res.innerHTML = inicio.value
+        }
+    }
 }
 
