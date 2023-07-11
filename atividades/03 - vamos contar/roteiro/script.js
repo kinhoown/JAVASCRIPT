f8a162e30730b804d0211b68d9cd69fca7f5fa11
Erick Alexandre <111152377+kinhoown@users.noreply.github.com>
2023-07-11 15:47:59 -0300

    finalizado

--- atividades/03 - vamos contar/roteiro/script.js
+++ atividades/03 - vamos contar/roteiro/script.js
@@ -8,12 +8,25 @@ function conta() {
     var passo = document.getElementById('passo')
     var res = document.getElementById('res')
 
-    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 || passo.value == 0){
         alert('[ERRO] Faltam dados!')
     }else{
-        for (inicio.value ; inicio.value < fim ; inicio.value =+ 1){
-            res.innerHTML = inicio.value
-        }
+        res.innerHTML = `Contando: `
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+
+        if(i < f){
+            for (var c = i ; c <= f ; c += p){
+                res.innerHTML += `${c} &#128073;`
+            }
+        
+        }else{
+            for (var c = i ; c >= f ; c -= p){
+                res.innerHTML += `${c} &#128073;`
+            }
+        res.innerHTML += `&#129313;`
     }
 }
 
+}
