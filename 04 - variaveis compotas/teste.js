5acfee8533766e8687fb7324047416b24235b822
Erick Alexandre <111152377+kinhoown@users.noreply.github.com>
2023-07-12 22:31:53 -0300

    att
    hj aprendi listas, arrey e function's.

---
+++ 04 - variaveis compotas/teste.js
@@ -0,0 +1,15 @@
+var variavel = []
+variavel.push('Erick')
+variavel.push('1')
+variavel.push('20')
+variavel.push('Livia')
+variavel.push('Jesus')
+variavel.push('margarete8')
+
+console.log(variavel)
+
+variavel.sort()
+
+console.log(variavel.length)
+
+console.log(variavel)
