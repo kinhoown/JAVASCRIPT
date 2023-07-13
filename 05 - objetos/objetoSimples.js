93491061a3f6d02db30e55542bbe14952a7e5ef9
Erick Alexandre <111152377+kinhoown@users.noreply.github.com>
2023-07-13 15:53:58 -0300

    att
    class e objetos simples

---
+++ 05 - objetos/objetoSimples.js
@@ -0,0 +1,13 @@
+var inputnome = 'Erick'
+var inputsobrenome = 'Alexandre'
+var inputidade = 25
+var inputsexo = 'Masculino'
+
+var pessoa = {
+    nome: inputnome ,
+    sobrenome: inputsobrenome , 
+    idade: inputidade ,
+    sexo: inputsexo
+}
+
+console.log(pessoa.sexo)
---
+++ 06 - class/teste.js
@@ -0,0 +1,14 @@
+class Pessoa {
+
+
+    nome = 'Erick'
+    idade = 25
+    sexo = 'Masculino'
+
+}
+
+var pessoa1 = new Pessoa()
+
+
+
+console.log(pessoa1)
