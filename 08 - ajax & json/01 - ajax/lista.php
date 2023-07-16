84e684d83d8ff3f7a235160440138e7c30f24154
Erick Alexandre <111152377+kinhoown@users.noreply.github.com>
2023-07-15 21:40:19 -0300

    att

---
+++ 08 - ajax & json/01 - ajax/lista.php
@@ -0,0 +1,26 @@
+<?php
+
+$usuarios = ["MARIA" , "JOAO"];
+
+echo json_encode($usuarios);
+
+
+/*
+"MARIA" , 
+    "GUILHERME" , 
+    "FLAVIA" , 
+    "MARCOS" , 
+    "JOAO" , 
+    "PEDRO" , 
+    "ESTEPHANE" , 
+    "VICTORIA" , 
+    "ANA" , 
+    "MARLENE" , 
+    "THOMPSON" , 
+    "FLAVIM MATADOR DE PORCO" , 
+    "JOSE ORISVALDO DA SILVA" , 
+    "CLEYTON RASTA" , 
+    "JESUS" , 
+    "MORGANA" , 
+    "ANASTACIA"
+*/
---
+++ 08 - ajax & json/01 - ajax/script.js
@@ -0,0 +1,15 @@
+/*
+var ajax = new XMLHttpRequest()
+
+ajax.open("GET", "lista.php")
+
+ajax.send()
+
+
+ajax.addEventListener("readystatechange", function(){
+
+    if (ajax.readyState === 4){
+        console.log(ajax)
+        console.log(ajax.response)
+    }
+})*/
