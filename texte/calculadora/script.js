dd3fcf515e6048aebb65f45a5731fabf56204b16
Erick Alexandre <111152377+kinhoown@users.noreply.github.com>
2023-08-02 15:52:17 -0300

    att

---
+++ texte/calculadora/script.js
@@ -0,0 +1,7 @@
+const resposta = document.querySelector('.resposta')
+const numeros = document.querySelectorAll('.numeros>div')
+
+
+numeros[0].addEventListener('click', () => {
+    console.log(numeros[0])
+})
