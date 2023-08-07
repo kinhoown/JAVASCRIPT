e48b2fe296399c75727061c36885c703e8a549c4
Erick Alexandre <111152377+kinhoown@users.noreply.github.com>
2023-08-07 13:49:48 -0300

    att

--- texte/calculadora/script.js
+++ texte/calculadora/script.js
@@ -3,14 +3,14 @@ document.addEventListener('click', (e) => {
     const res = document.querySelector('.resposta')
     const el = e.target
 
-    console.log(cont)
     if (el.classList.contains('numeros')){
-        console.log(cont)
         cont += el.value
         res.innerHTML += el.value
         console.log(Number(cont))
     }else if (el.classList.contains('operadores')){
-
+        cont += el.value
+        res.innerHTML += el.value
+        console.log(Number(cont))
     }else if (el.classList.contains('limpar')){
 
     }else if (el.classList.contains('apagar')){
